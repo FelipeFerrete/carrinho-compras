5a6803f3e95c0a7968237984590a908f31df4fee
FelipeFerrete <felipeferretelemes@gmail.com>
2025-04-03 15:31:44 -0300

    add carrinho / valor

---
+++ js/app.js
@@ -0,0 +1,32 @@
+    //Recuperar valores - nome,qnt e valor 
+    //Calcular preco, subtotal
+    //add carrinho
+    //atualizar valor total
+
+    let totalGeral =0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$ 0'
+    
+function adicionar(){
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1];
+    let quantidade = document.getElementById('quantidade').value; 
+    //calc
+    let preco = quantidade * valorUnitario;
+    let carrinho = document.getElementById('lista-produtos');
+    //addCarrinho
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+        </section>`;
+    //attValor
+    totalGeral = totalGeral + preco;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$ ${totalGeral}`
+        
+    }
+
+
+function limpar(){
+
+}
